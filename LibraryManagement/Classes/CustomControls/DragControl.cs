877879db494da6dc3a54988204bc6746d8c320de
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2018-06-23 14:41:04 -0400

    Added Sliding Function to Sidepanel
    Added a sliding function to Sidepanel. speed of movement is determined by the distance between current position of Sidepanel and the clicked button

---
+++ LibraryManagement/Classes/CustomControls/DragControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace LibraryManagement
+{
+    class DragControl : Component
+    {
+        private Control handleControl;
+
+        public Control SelectControl
+        {
+            get { return handleControl; }
+            set
+            {
+                handleControl = value;
+                handleControl.MouseDown += new MouseEventHandler(DragForm_MouseDown);
+            }
+        }
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr a, int msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void DragForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            bool flag = e.Button == MouseButtons.Left;
+
+            if (flag)
+            {
+                ReleaseCapture();
+                SendMessage(SelectControl.FindForm().Handle, 161, 2, 0);
+            }
+        }
+    }
+}
---
+++ LibraryManagement/Classes/CustomControls/GradientPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace LibraryManagement
+{
+    public class GradientPanel : Panel
+    {
+        public GradientPanel()
+        {
+            DoubleBuffered = true;
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        public float Angle { get; set; }
+        public Color TopColor { get; set; }
+        public Color BottomColor { get; set; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            LinearGradientBrush lgb = new LinearGradientBrush(ClientRectangle, TopColor, BottomColor, Angle);
+
+            Graphics g = e.Graphics;
+            g.FillRectangle(lgb, ClientRectangle);
+
+            base.OnPaint(e);
+        }
+    }
+}
